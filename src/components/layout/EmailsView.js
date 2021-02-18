fc3363b25e92007430dc657f79d0cad73e537847
KRVaghani <kaushik.vaghani111@gmail.com>
2021-02-18 16:18:00 +0530

    first commit

---
+++ src/components/layout/EmailsView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmailsView = () => {
+    return (
+        <div>
+            emailview
+        </div>
+    )
+}
+
+export default EmailsView
---
+++ src/components/layout/SideIcons.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideIcons = () => {
+    return (
+        <div>
+            sideicons
+        </div>
+    )
+}
+
+export default SideIcons
